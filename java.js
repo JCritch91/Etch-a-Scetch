31b2be6a4553dfdf3f9e2c3831dfccbd34e4349d
Jamie <134149238+JCritch91@users.noreply.github.com>
2023-06-13 10:33:36 +0100

    Create logic to randomise colours Create logic to switch back to black only Create logic to chnage colours to white add css formatting

--- java.js
+++ java.js
@@ -1,15 +1,65 @@
 const chooseSize = document.querySelector('#grid-prompt');
 const gridContainer = document.querySelector('#grid-container');
+const ranColour = document.querySelector('#random-colour');
+const blkColour = document.querySelector('#black-colour');
+const eraser = document.querySelector('#eraser');
+let gridNumber = 0;
+let comboNumber = 0;
+let id = 0;
+let randomColour = "false";
+let blackColour = "true";
+let whiteColour = "false";
+let r = 0;
+let g = 0;
+let b = 0;
+
+ranColour.addEventListener('click', () =>
+{ randomColour = "true";
+  blackColour = "false";
+  whiteColour = "false";
+  ranColour.style.backgroundColor ='blue'
+  blkColour.style.backgroundColor ='white'
+  eraser.style.backgroundColor ='white'
+  colourSelect();
+})
+
+blkColour.addEventListener('click', () =>
+{ randomColour = "false";
+  blackColour = "true";
+  whiteColour = "false"
+  ranColour.style.backgroundColor ='white'
+  blkColour.style.backgroundColor ='blue'
+  eraser.style.backgroundColor ='white'
+  r = 0;
+  g = 0;
+  b = 0;
+})
+
+eraser.addEventListener('click', () =>
+{ randomColour = "false";
+  blackColour = "false";
+  whiteColour = "true"
+  ranColour.style.backgroundColor ='white'
+  blkColour.style.backgroundColor ='white'
+  eraser.style.backgroundColor ='blue'
+  r = 255;
+  g = 255;
+  b = 255;
+})
+
 chooseSize.addEventListener('click', () => 
 {let promptNumber = prompt("Choose Grid Size From 1 - 100");
 resetGrid();
 gridNumber = 0;
+comboNumber = 0;
+ranColour.style.backgroundColor ='white'
+blkColour.style.backgroundColor ='white'
+eraser.style.backgroundColor ='white'
 validateNumber(promptNumber);
-
 }); 
 
-let gridNumber = 0
-let comboNumber = 0
+
+
 
 function validateNumber(promptNumber){
     if (isNaN(+promptNumber)){
@@ -31,14 +81,15 @@ function validateNumber(promptNumber){
 function createGrid() {
     for (let i=0; i < comboNumber; i++){
         const newDiv = document.createElement('div');
-        const boxSize = 500 / gridNumber;
+        const boxSize = 400 / gridNumber;
         newDiv.setAttribute('id', i);
         newDiv.classList.add('grid-box');
-        newDiv.textContent = (i);
+        //newDiv.textContent = (i);
         newDiv.style.width = boxSize+'px';
         newDiv.style.height =boxSize+'px';
         gridContainer.append(newDiv);
     }
+
 }
 
 function resetGrid() {
@@ -48,5 +99,27 @@ function resetGrid() {
     }  
     
 }
-        
-    
+
+const onClick = (event) => {
+    let selectId = event.srcElement.id
+    if (selectId > -1) {
+    const reFormat = document.getElementById(selectId);
+    if (randomColour === "true"){
+        reFormat.style.backgroundColor = "rgb("+r+","+g+","+b+")"
+        colourSelect();
+    } else if(blackColour === "true"){
+        reFormat.style.backgroundColor = "rgb("+r+","+g+","+b+")"
+    } else if(whiteColour ==="true"){
+        reFormat.style.backgroundColor = "rgb("+r+","+g+","+b+")"
+    }
+    }
+    console.log(event.srcElement.id);
+  }
+  window.addEventListener('mouseover', onClick);
+
+function colourSelect(){
+    r = Math.floor(Math.random()*255)+1;
+    g = Math.floor(Math.random()*255)+1;
+    b = Math.floor(Math.random()*255)+1;
+    return;
+}
