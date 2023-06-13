5a4544c11cd7b3c442dc8be2bd380791c5579bc1
Jamie <134149238+JCritch91@users.noreply.github.com>
2023-06-13 15:53:56 +0100

    Add logic for progressive gradient Edit styles

--- java.js
+++ java.js
@@ -2,64 +2,91 @@ const chooseSize = document.querySelector('#grid-prompt');
 const gridContainer = document.querySelector('#grid-container');
 const ranColour = document.querySelector('#random-colour');
 const blkColour = document.querySelector('#black-colour');
+const prog = document.querySelector('#progressive');
 const eraser = document.querySelector('#eraser');
+const eraseAll = document.querySelector('#erase-all');
+
 let gridNumber = 0;
 let comboNumber = 0;
 let id = 0;
 let randomColour = "false";
 let blackColour = "true";
 let whiteColour = "false";
+let progress = "false";
 let r = 0;
 let g = 0;
 let b = 0;
-
-ranColour.addEventListener('click', () =>
-{ randomColour = "true";
-  blackColour = "false";
-  whiteColour = "false";
-  ranColour.style.backgroundColor ='blue'
-  blkColour.style.backgroundColor ='white'
-  eraser.style.backgroundColor ='white'
-  colourSelect();
-})
-
-blkColour.addEventListener('click', () =>
-{ randomColour = "false";
-  blackColour = "true";
-  whiteColour = "false"
-  ranColour.style.backgroundColor ='white'
-  blkColour.style.backgroundColor ='blue'
-  eraser.style.backgroundColor ='white'
-  r = 0;
-  g = 0;
-  b = 0;
-})
-
-eraser.addEventListener('click', () =>
-{ randomColour = "false";
-  blackColour = "false";
-  whiteColour = "true"
-  ranColour.style.backgroundColor ='white'
-  blkColour.style.backgroundColor ='white'
-  eraser.style.backgroundColor ='blue'
-  r = 255;
-  g = 255;
-  b = 255;
-})
 
 chooseSize.addEventListener('click', () => 
 {let promptNumber = prompt("Choose Grid Size From 1 - 100");
 resetGrid();
 gridNumber = 0;
 comboNumber = 0;
-ranColour.style.backgroundColor ='white'
-blkColour.style.backgroundColor ='white'
-eraser.style.backgroundColor ='white'
 validateNumber(promptNumber);
+ranColour.style.backgroundColor ='crimson'
+blkColour.style.backgroundColor ='crimson'
+eraser.style.backgroundColor ='crimson'
+blkColour.style.backgroundColor ='grey'
 }); 
 
+blkColour.addEventListener('click', () =>
+{ randomColour = "false";
+  blackColour = "true";
+  whiteColour = "false"
+  ranColour.style.backgroundColor ='crimson'
+  blkColour.style.backgroundColor ='grey'
+  eraser.style.backgroundColor ='crimson'
+  prog.style.backgroundColor ='crimson'
+  r = 0;
+  g = 0;
+  b = 0;
+})
 
+prog.addEventListener('click', () =>
+{   randomColour = "false";
+    blackColour = "false";
+    whiteColour = "false";
+    progress = "true";
+    ranColour.style.backgroundColor ='crimson'
+    blkColour.style.backgroundColor ='crimson'
+    eraser.style.backgroundColor ='crimson'
+    prog.style.backgroundColor ='grey'
+    progressive();
+})
 
+ranColour.addEventListener('click', () =>
+{ randomColour = "true";
+  blackColour = "false";
+  whiteColour = "false";
+  ranColour.style.backgroundColor ='grey'
+  blkColour.style.backgroundColor ='crimson'
+  eraser.style.backgroundColor ='crimson'
+  prog.style.backgroundColor ='crimson'
+  colourSelect();
+})
+
+eraser.addEventListener('click', () =>
+{ randomColour = "false";
+  blackColour = "false";
+  whiteColour = "true"
+  ranColour.style.backgroundColor ='crimson'
+  blkColour.style.backgroundColor ='crimson'
+  eraser.style.backgroundColor ='grey'
+  prog.style.backgroundColor ='crimson'
+  r = 255;
+  g = 255;
+  b = 255;
+})
+
+eraseAll.addEventListener('click', () =>
+{ for (let i=0; i < comboNumber; i++){
+    const erase = document.getElementById(i)
+    r = 255;
+    g = 255;
+    b = 255;
+    erase.style.backgroundColor = "rgb("+r+","+g+","+b+")"
+    }
+})
 
 function validateNumber(promptNumber){
     if (isNaN(+promptNumber)){
@@ -84,12 +111,11 @@ function createGrid() {
         const boxSize = 400 / gridNumber;
         newDiv.setAttribute('id', i);
         newDiv.classList.add('grid-box');
-        //newDiv.textContent = (i);
         newDiv.style.width = boxSize+'px';
         newDiv.style.height =boxSize+'px';
+        newDiv.style.backgroundColor = "rgb(255,255,255)"
         gridContainer.append(newDiv);
     }
-
 }
 
 function resetGrid() {
@@ -97,25 +123,30 @@ function resetGrid() {
     const removeGrid = document.getElementById(i)
     removeGrid.remove()
     }  
-    
 }
 
-const onClick = (event) => {
+const onHover = (event) => {
     let selectId = event.srcElement.id
+    id = selectId
     if (selectId > -1) {
-    const reFormat = document.getElementById(selectId);
-    if (randomColour === "true"){
-        reFormat.style.backgroundColor = "rgb("+r+","+g+","+b+")"
-        colourSelect();
-    } else if(blackColour === "true"){
-        reFormat.style.backgroundColor = "rgb("+r+","+g+","+b+")"
-    } else if(whiteColour ==="true"){
-        reFormat.style.backgroundColor = "rgb("+r+","+g+","+b+")"
+        const reFormat = document.getElementById(selectId);
+        if (randomColour === "true"){
+            reFormat.style.backgroundColor = "rgb("+r+","+g+","+b+")"
+            colourSelect();
+        } else if(blackColour === "true"){
+            reFormat.style.backgroundColor = "rgb("+r+","+g+","+b+")"
+        } else if(whiteColour ==="true"){
+            reFormat.style.backgroundColor = "rgb("+r+","+g+","+b+")"
+        } else if(progress ==="true"){
+            progressive()
+            r -= 25.5
+            g -= 25.5
+            b -= 25.5
+            reFormat.style.backgroundColor = "rgb("+r+","+g+","+b+")"
+        }
     }
-    }
-    console.log(event.srcElement.id);
   }
-  window.addEventListener('mouseover', onClick);
+
 
 function colourSelect(){
     r = Math.floor(Math.random()*255)+1;
@@ -123,3 +154,38 @@ function colourSelect(){
     b = Math.floor(Math.random()*255)+1;
     return;
 }
+
+function progressive(event){
+    let element = document.getElementById(id)
+    let color = element.style.getPropertyValue("background-color")
+    let rgbColors=new Object();
+
+    //if RGB scheme
+    if (color[0]=='r'){
+    color=color.substring(color.indexOf('(')+1, color.indexOf(')'));
+    rgbColors=color.split(',', 3);
+    // Convert redValue to integer
+    rgbColors[0]=parseInt(rgbColors[0]);
+    // Convert greenValue to integer
+    rgbColors[1]=parseInt(rgbColors[1]);
+    // Convert blueValue to integer
+    rgbColors[2]=parseInt(rgbColors[2]);		
+  }
+    //if # format
+    else if (color.substring(0,1)=="#")
+    {
+        rgbColors[0]=color.substring(1, 3);  // redValue
+        rgbColors[1]=color.substring(3, 5);  // greenValue
+        rgbColors[2]=color.substring(5, 7);  // blueValue
+        
+        //convert to integer
+        rgbColors[0]=parseInt(rgbColors[0], 16);
+        rgbColors[1]=parseInt(rgbColors[1], 16);
+        rgbColors[2]=parseInt(rgbColors[2], 16);
+	}
+    r = rgbColors[0]
+    g = rgbColors[1]
+    b = rgbColors[2]
+}
+
+gridContainer.addEventListener('mouseover', onHover);
